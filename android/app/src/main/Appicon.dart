7149e34bdb0fa85c31ef59df3a927f1cd0495808
Dharmendra <78801686+ProgrammingPirates@users.noreply.github.com>
2021-10-09 07:35:30 +0530

    App  Icon

---
+++ android/app/src/main/Appicon.dart
@@ -0,0 +1,21 @@
+Row(
+  mainAxisAlignment: MainAxisAlignment.spaceAround,
+  children: const <Widget>[
+    Icon(
+      Icons.favorite,
+      color: Colors.pink,
+      size: 24.0,
+      semanticLabel: 'Text to announce in accessibility modes',
+    ),
+    Icon(
+      Icons.audiotrack,
+      color: Colors.green,
+      size: 30.0,
+    ),
+    Icon(
+      Icons.beach_access,
+      color: Colors.blue,
+      size: 36.0,
+    ),
+  ],
+)
